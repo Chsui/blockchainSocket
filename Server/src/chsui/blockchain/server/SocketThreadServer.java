ae325a44b70a88537691629f2105c72774c3b5ab
Chsui <choco.nina.mc@gmail.com>
2022-04-25 14:52:50 +0900

    add SocketThreadServer

---
+++ Server/src/chsui/blockchain/server/SocketThreadServer.java
@@ -0,0 +1,43 @@
+package chsui.blockchain.server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class SocketThreadServer extends Thread {
+    private Socket socket;
+
+    public SocketThreadServer(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run() {
+        BufferedReader in = null;
+        PrintWriter out = null;
+        try {
+            String connIp = socket.getInetAddress().getHostAddress();
+            System.out.println(connIp + "에서 연결 시도");
+
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream());
+
+            String command = in.readLine();
+            System.out.println("[" + connIp + "] Run Command: " + command);
+
+            out.write(Main.exec(command));
+            out.flush();
+        } catch(IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if(out != null) out.close();
+                if(in != null) in.close();
+                if(socket != null) socket.close();
+            }  catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
